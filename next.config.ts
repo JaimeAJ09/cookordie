96f16a93f00d090620c99ce5a9ef1f7746a94649
JAIME.ALVAREZ <jaime.alvarez@Mac.home>
2026-02-18 12:44:59 +0000

    fix whatsapp loading and dns prefetch

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+  reactStrictMode: true,
+  poweredByHeader: false,
+  compress: true,
+  images: {
+    formats: ['image/avif', 'image/webp'],
+  },
+  experimental: {
+    optimizeCss: true,
+  },
 };
 
 export default nextConfig;
+
